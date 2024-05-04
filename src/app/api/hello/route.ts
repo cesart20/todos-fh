5c2867d93338c5b85fecded872b63fdd9910995b
Ctorres2020 <cesarivantorres20@gmail.com>
2024-05-04 19:54:56 -0400

    first commit

---
+++ src/app/api/hello/route.ts
@@ -0,0 +1,8 @@
+import { NextResponse, NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+    
+    return NextResponse.json({
+        Hola: 'Mundo',
+    })
+}
